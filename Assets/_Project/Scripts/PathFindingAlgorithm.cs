fe10c937d36bf760b83d4882f269a297594bfb1b
Dante <martin.velazquez.luis.alfonso@gmail.com>
2023-11-07 12:39:01 -0600

    Path Finding RealTime

--- Assets/_Project/Scripts/PathFindingAlgorithm.cs
+++ Assets/_Project/Scripts/PathFindingAlgorithm.cs
@@ -2,37 +2,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class PathFindingAlgorithm : MonoBehaviour
+public static class PathFindingAlgorithm
 {
-    private PriorityQueue<Vector3Int> _frontier = new PriorityQueue<Vector3Int>();
-    private Dictionary<Vector3Int, Vector3Int?> _cameFrom = new Dictionary<Vector3Int, Vector3Int?>();
-    private Dictionary<Vector3Int, int> _costSoFar = new Dictionary<Vector3Int, int>();
+    private static Queue<Vector3Int> _frontier = new Queue<Vector3Int>();
+    private static PriorityQueue<Vector3Int> _frontierPriority = new PriorityQueue<Vector3Int>();
+    private static Dictionary<Vector3Int, Vector3Int?> _cameFrom = new Dictionary<Vector3Int, Vector3Int?>();
+    private static Dictionary<Vector3Int, int> _costSoFar = new Dictionary<Vector3Int, int>();
+    private static HashSet<Vector3Int> _reached = new HashSet<Vector3Int>();
 
-    public List<Vector3Int> PathFinding(Vector3Int originPos, Vector3Int objectivePos, Tilemap tileMap)
+    public static List<Vector3Int> PathFinding(Vector3Int originPos, Vector3Int objectivePos, Tilemap tileMap)
     {
         RestartPathFinding();
 
-        _frontier.Enqueue(originPos, 0);
+        _frontierPriority.Enqueue(originPos, 0);
         _cameFrom.Add(originPos, null);
         _costSoFar.Add(originPos, 0);
 
-        while (_frontier.Count > 0)
+        while (_frontierPriority.Count > 0)
         {
             bool endEarly = false;
 
-            var current = _frontier.Dequeue();
+            var current = _frontierPriority.Dequeue();
 
             foreach (var next in tileMap.GetNeighbours(current))
             {
                 //Si no es customTile, no tiene peso, no usar
                 var nextTile = tileMap.GetTile(next) as CustomTile;
                 if (nextTile == null) continue;
-
-                if (next == objectivePos)
-                {
-                    endEarly = true;
-                    break;
-                }
 
                 var newCost = _costSoFar[current] + nextTile.Cost;
 
@@ -41,7 +37,7 @@ public class PathFindingAlgorithm : MonoBehaviour
                 {
                     _costSoFar.Add(next, newCost);
                     var priority = newCost + Heuristic(objectivePos, next);
-                    _frontier.Enqueue(next, priority);
+                    _frontierPriority.Enqueue(next, priority);
                     _cameFrom.Add(next, current);
                 }
 
@@ -66,22 +62,49 @@ public class PathFindingAlgorithm : MonoBehaviour
             from = _cameFrom[from.Value];
         }
 
-        return pathToFollow;
+        return new List<Vector3Int>(pathToFollow);
     }
 
-    public void FillLimited(int tilesToFill)
+    public static List<Vector3Int> FillLimited(Vector3Int originPos ,int tilesToFill, Tilemap tileMap)
     {
+        RestartPathFinding();
+        
+        _frontier.Enqueue(originPos);
+        _reached.Add(originPos);
+        _costSoFar.Add(originPos, tilesToFill);
+        
+        while (_frontier.Count > 0)
+        {
+            var current = _frontier.Dequeue();
+            foreach (var neighbour in tileMap.GetNeighbours(current))
+            {
+                //Si no se ha alcanzado el vecino, se a�ade a la frontera y a los tiles alcanzados
+                if (_reached.Contains(neighbour)) continue;
+                
+                var tile = tileMap.GetTile(neighbour) as CustomTile;
+                var tileCost = tile == null ? 1 : tile.Cost;
 
+                var newCost = _costSoFar[current] - tileCost;
+                if(newCost < 0) continue;
+                
+                _frontier.Enqueue(neighbour);
+                _reached.Add(neighbour);
+                _costSoFar[neighbour] = newCost;
+            }
+        }
+        
+        return new List<Vector3Int>(_reached);
     }
 
-    private int Heuristic(Vector3Int a, Vector3Int b)
+    private static int Heuristic(Vector3Int a, Vector3Int b)
     {
         return Mathf.Abs(a.x - b.x) + Mathf.Abs(a.y - b.y);
     }
 
-    private void RestartPathFinding() 
+    private static void RestartPathFinding() 
     {
-        _frontier.Clear();
+        _frontierPriority.Clear();
+        _reached.Clear();
         _cameFrom.Clear();
         _costSoFar.Clear();
     }
